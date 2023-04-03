478d90cbb34c9985d7edac1b1a441e2ee8eb8afa
Omar Altali <oaltali.e@gmail.com>
2023-04-03 05:32:43 +0300

    remove redundant test code

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -33,7 +33,6 @@ class FeedStore {
     
     typealias DeletionCompletion = (Error?) -> Void
     var deleteCachedFeedCallCount = 0
-    var insertCallCount = 0
     var insertions = [(items: [FeedItem], timestamp: Date )]()
     
     private var deletionCompletions = [DeletionCompletion]()
@@ -52,7 +51,6 @@ class FeedStore {
     }
     
     func insert(_ items: [FeedItem], timestamp: Date) {
-        insertCallCount += 1
         insertions.append((items, timestamp))
     }
     
@@ -82,21 +80,9 @@ class CacheFeedUseCaseTests: XCTestCase {
         sut.save(items)
         store.completeDeletion(with: deletionError)
         
-        XCTAssertEqual(store.insertCallCount, 0)
+        XCTAssertEqual(store.insertions.count, 0)
     }
-    
-    func test_save_requestsNewCacheInsertionONSuccessfullDeletion() {
-        let (sut, store) = makeSUT()
-        let items = [uniqueItem(), uniqueItem()]
         
-        sut.save(items)
-        store.completeDeletionSuccessfully()
-        
-        XCTAssertEqual(store.insertCallCount, 1)
-
-        
-    }
-    
     func test_save_requestsNewCacheInsertionWithTimestampONSuccessfullDeletion() {
         let timestamp = Date()
         let (sut, store) = makeSUT(currentDate: {timestamp})
@@ -108,9 +94,7 @@ class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.insertions.count, 1)
         XCTAssertEqual(store.insertions.first?.items, items)
         XCTAssertEqual(store.insertions.first?.timestamp, timestamp)
-        
 
-        
     }
 
     
