d161db52c40ad07338060cdccbc41af6ffcc5ec3
omar-altali <o.altali@foodics.com>
2023-10-20 06:37:45 +0300

    move to seperate folders

---
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,49 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Omar Altali on 20/10/2023.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+
+    private let store: FeedStore
+    private let currentDate: () -> Date
+
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> ()) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else {return}
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+
+    private func cache(_ items: [FeedItem],with completion: @escaping (Error?) -> Void) { store.insert(items, timestamp: currentDate(), completion: {[weak self] error in
+        guard self != nil else {return}
+        completion(error)})
+    }
+
+
+}
+
+public protocol FeedStore {
+
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+
+}
+
