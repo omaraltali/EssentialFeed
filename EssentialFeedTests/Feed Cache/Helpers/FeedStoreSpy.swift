3da6f09563fef434a94e49d4f41d5883e569c5ac
omaraltali <oaltali.e@gmail.com>
2023-10-21 02:01:43 +0300

    move FeedStoreSpy to seperate file

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+
+    enum ReceivedMessage: Equatable {
+        case delteCachedFeed
+        case insert([LocalFeedImage], Date)
+        case insertionError
+    }
+
+    private(set) var receivedMessages = [ReceivedMessage]()
+
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.delteCachedFeed)
+    }
+
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+
+    func completeInsertion(with error: Error, at index: Int = 0){
+        insertionCompletions[index](error)
+    }
+
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+
+}
