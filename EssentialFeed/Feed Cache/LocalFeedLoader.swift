cda4efa9e4a6d1cd18631c860078509b8bdd12e5
omaraltali <oaltali.e@gmail.com>
2023-10-21 05:44:41 +0300

    Load Command delivers no images on 7 days old cache

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -32,24 +32,33 @@ public final class LocalFeedLoader {
     }
 
     public func load (completion: @escaping (LoadFeedResult) -> Void) {
-//        store.retrieve{error in completion(error)}
-        store.retrieve { error in
-            if let error = error {
+        //store.retrieve{error in completion(error)}
+        store.retrieve { [unowned self] result in
+            switch result {
+            case let .failure(error):
                 completion(.faliure(error))
-            } else {
+            case let .found(feed,timestamp) where self.validate(timestamp):
+                completion(.success(feed.toModels()))
+            case .found, .empty:
                 completion(.success([]))
             }
-
-
         }
     }
 
-    
+    private func validate(_ timestamp: Date) -> Bool {
+        let calendar = Calendar(identifier: .gregorian)
+       guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
+    }
+
+
 
     private func cache(_ feed: [FeedImage],with completion: @escaping (SaveResult) -> Void) {
         store.insert(feed.toLocal(), timestamp: currentDate(), completion: {[weak self] error in
-        guard self != nil else {return}
-        completion(error)})
+            guard self != nil else {return}
+            completion(error)})
     }
 }
 
@@ -58,3 +67,10 @@ private extension Array where Element == FeedImage {
         return map {LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
     }
 }
+private extension Array where Element == LocalFeedImage {
+
+    func toModels() -> [FeedImage] {
+        return map{FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
+    }
+}
+
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -40,7 +40,28 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         expect(sut, toCompleteWith: .success([])) {
             store.completeRetrievalWithEmptyCache()
         }
+    }
 
+    func test_load_deliverCachedImagesOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: {fixedCurrentDate } )
+
+        expect(sut, toCompleteWith: .success(feed.models)) {
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        }
+    }
+
+    func test_load_deliverNoImagesOnSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT(currentDate: {fixedCurrentDate } )
+
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
+        }
     }
 
 
@@ -86,5 +107,33 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
        return NSError(domain: "any error", code: 0)
     }
 
+    private func anyURL() -> URL {
+        return URL(string:"https://any-url.com")!
+    }
 
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueImage(), uniqueImage()]
+        let local = models.map{LocalFeedImage(id:$0.id, description: $0.description, location: $0.location, url: $0.url)}
+        return (models, local)
+    }
+
+
+
+
+
+}
+
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
 }
