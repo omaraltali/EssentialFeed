244b07d7f939a0c6d0977f13348c4854be6692bb
omaraltali <oaltali.e@gmail.com>
2023-12-23 04:44:09 +0300

    move feed cache max age ( 7 days ) to a computed var to calirify intent in code

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -21,8 +21,11 @@ func uniqueImage() -> FeedImage {
 
 extension Date {
 
+    private var feedCacheMaxAgeInDays: Int {
+        return 7
+    }
     func minusFeedCacheMaxAge() -> Date {
-        return adding(days: -7)
+        return adding(days: -feedCacheMaxAgeInDays)
     }
     func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
