0034919fb2cf30eabccad99b9a2abaccf7761f56
omaraltali <oaltali.e@gmail.com>
2024-01-15 04:30:48 +0300

    Add CoreDataFeedStore and create core data models with their models in code + background process context + makeSUT

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,72 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Omar Altali on 13/01/2024.
+//
+
+import XCTest
+import EssentialFeed
+
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retreieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+
+    }
+    
+    func test_storeSideEffects_runSerially() {
+
+    }
+    
+    // - MARK: Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+}
