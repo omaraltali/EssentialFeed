42001354755c95e456a224a37ced8fe064e09ed8
omaraltali <oaltali.e@gmail.com>
2024-01-16 04:14:32 +0300

    Add EssentialFeedCacheIntergrationTest target

---
+++ EssentialFeedCacheIntergrationTests/EssentialFeedCacheIntergrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntergrationTests.swift
+//  EssentialFeedCacheIntergrationTests
+//
+//  Created by Omar Altali on 16/01/2024.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntergrationTests: XCTestCase {
+
+
+}
