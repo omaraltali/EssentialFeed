219a3dd69fe215ee6c65102f02400a31bea64a09
omaraltali <oaltali.e@gmail.com>
2024-01-28 00:44:07 +0300

    Render Loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Omar Altali on 27/01/2024.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -10,6 +10,7 @@ import EssentialFeed
 
 final public class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
+    private var tableModel = [FeedImage]()
 
     public convenience init(loader: FeedLoader) {
         self.init()
@@ -32,8 +33,32 @@ final public class FeedViewController: UITableViewController {
 
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load(completion: { [weak self] _ in
+        loader?.load(completion: { [weak self] result in
+//            switch result {
+//            case .success(let feedImage):
+//                self?.tableModel = feedImage
+//            case .failure(let e):
+//                break
+//            default:
+//                break
+//            }
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
             self?.refreshControl?.endRefreshing()
         })
+    }
+
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
     }
 }
