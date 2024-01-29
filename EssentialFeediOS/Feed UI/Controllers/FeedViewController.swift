a78d10ec573d140e3c1a68826a3c3db60a30e223
omaraltali <oaltali.e@gmail.com>
2024-01-30 02:19:15 +0300

    Extract FeedImageCell creation and configration and image loading logic with "FeedImageDataLoader" to the new "FeedImageController"

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -16,7 +16,8 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     private var tableModel = [FeedImage]() {
         didSet { tableView.reloadData() }
     }
-    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
+
+    private var cellControllers = [IndexPath: FeedImageCellController]()
 
     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         self.init()
@@ -24,16 +25,16 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
         self.imageLoader = imageLoader
     }
 
-   public override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
-       self.refreshControl = refreshController?.view
-       refreshController?.onRefresh = { [weak self] feed in
-           self?.tableModel = feed
-       }
+        self.refreshControl = refreshController?.view
+        refreshController?.onRefresh = { [weak self] feed in
+            self?.tableModel = feed
+        }
 
-       tableView.prefetchDataSource = self
-       refreshController?.refresh()
+        tableView.prefetchDataSource = self
+        refreshController?.refresh()
 
     }
 
@@ -48,49 +49,32 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellModel = tableModel[indexPath.row]
-
-        let cell = FeedImageCell()
-        cell.locationContainer.isHidden = (cellModel.location == nil)
-        cell.locationLabel.text = cellModel.location
-        cell.descriptionLabel.text = cellModel.description
-        cell.feedImageView.image = nil
-        cell.feedImageRetryButton.isHidden = true
-        cell.feedImageContainer.startShimmering()
-        let loadImage =  { [weak self, weak cell] in
-            guard let self else {return}
-            self.tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) {[weak cell] result in
-                let data = try? result.get()
-                cell?.feedImageView.image = data.map(UIImage.init) ?? nil
-                cell?.feedImageRetryButton.isHidden = (data != nil)
-                cell?.feedImageContainer.stopShimmering()
-            }
-        }
-
-        cell.onRetry = loadImage
-        loadImage()
-
-        return cell
+        return cellController(forRowAt: indexPath).view()
     }
 
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let cellModel = tableModel[indexPath.row]
-            tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) {_ in}
+            cellController(forRowAt: indexPath).preload()
         }
     }
 
 
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cancelTask(forRowAt: indexPath)
+        removeCellController(forRowAt: indexPath)
     }
 
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
-        indexPaths.forEach(cancelTask)
+        indexPaths.forEach(removeCellController)
     }
 
-    private func cancelTask(forRowAt indexPath: IndexPath) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+    private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+        let cellModel = tableModel[indexPath.row]
+        let cellController = FeedImageCellController(model: cellModel, imageLoader: imageLoader!)
+        cellControllers[indexPath] = cellController
+        return cellController
+    }
+
+    private func removeCellController(forRowAt indexPath: IndexPath) {
+        cellControllers[indexPath] = nil
     }
 }
