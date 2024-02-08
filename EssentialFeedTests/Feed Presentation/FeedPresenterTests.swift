79a24fc94084e7e63635235895b5455e872b6a93
omaraltali <oaltali.e@gmail.com>
2024-02-08 03:35:31 +0300

    test_title_isLocalized() Pass

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -46,6 +46,14 @@ class FeedPresenter {
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
 
+
+    static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view")
+    }
+
     private var feedLoadError: String {
         return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
             tableName: "Feed",
@@ -77,6 +85,10 @@ class FeedPresenter {
 
 final class FeedPresenterTests: XCTestCase {
 
+    func test_title_isLocalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
+    
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
