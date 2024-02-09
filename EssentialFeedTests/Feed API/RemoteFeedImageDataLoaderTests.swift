055afc3a6fb399a3f1892a0df8cae61396dcbd14
omaraltali <oaltali.e@gmail.com>
2024-02-10 02:05:12 +0300

    test_loadImageDataFromURLTwice_requestsDataFromURLTwice pass

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -37,8 +37,15 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
 
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
 
+        sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
 
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
 
 
 
