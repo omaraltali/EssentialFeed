a40f5ee1290ce3d4174d518972c6e9a551b2d736
omaraltali <oaltali.e@gmail.com>
2024-02-12 15:39:18 +0300

    test_cancelGetFromURLTask_cancelsURLRequest Pass

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -7,16 +7,27 @@
 
 import Foundation
 
-public class URLSessionHTTPClient: HTTPClient {
+public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
     public init(session: URLSession = .shared) {
         self.session = session
     }
    private struct UnexpectedvaluesRepresentation: Error {}
-    
-    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> ()){
-        session.dataTask(with: url) { data, response, error in
+
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        
+        let wrapped: URLSessionTask
+
+        func cancel() {
+            wrapped.cancel()
+        }
+        
+        
+    }
+
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+       let task = session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
             } else if let data = data, let response = response as? HTTPURLResponse {
@@ -27,6 +38,7 @@ public class URLSessionHTTPClient: HTTPClient {
             }
             
         }
-        .resume()
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
     }
 }
