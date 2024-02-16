7dab432fd7789f26b53fe36d4481b46cabe65ada
omaraltali <oaltali.e@gmail.com>
2024-02-17 01:17:58 +0300

    Refactor switch statement with map and flatMap chain

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -16,6 +16,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     }
 
     public enum Error: Swift.Error {
+        case connectivity
         case invalidData
     }
 
@@ -42,20 +43,30 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         }
     }
 
+    @discardableResult
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         let task = HTTPClientTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
-            guard let self else { return }
+            guard  self != nil else { return }
 
-            switch result {
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error): task.complete(with: .failure(error))
-            }
+//            switch result {
+//            case let .success((data, response)):
+//                if response.statusCode == 200, !data.isEmpty {
+//                    task.complete(with: .success(data))
+//                } else {
+//                    task.complete(with: .failure(Error.invalidData))
+//                }
+//            case .failure:
+//                task.complete(with: .failure(Error.connectivity))
+//            }
+
+            task.complete(with: result
+                .mapError{_ in Error.connectivity}
+                .flatMap({ data, response in
+                    let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
+            )
         }
         return task
     }
