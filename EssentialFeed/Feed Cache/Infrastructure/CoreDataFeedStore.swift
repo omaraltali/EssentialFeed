0b671a84e3f722a0e2334deb5b0fe77fab219d82
omaraltali <oaltali.e@gmail.com>
2024-02-23 02:59:12 +0300

    test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL Pass

--- EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
@@ -64,7 +64,7 @@ public final class CoreDataFeedStore: FeedStore {
 
     }
 
-    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
         context.perform { action(context) }
     }
@@ -72,10 +72,20 @@ public final class CoreDataFeedStore: FeedStore {
 
 extension CoreDataFeedStore: FeedImageDataStore {
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+        perform { context in
+            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
 
+            image.data = data
+            try? context.save()
+        }
     }
 
     public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(.none))
+        perform { context in
+            completion(Result {
+                return try ManagedFeedImage.first(with: url, in: context)?.data
+            })
+        }
     }
 }
