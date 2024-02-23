1d54dfeddaa5d406c18731b46051e63e3daae43a
omaraltali <oaltali.e@gmail.com>
2024-02-23 04:07:20 +0300

    LocalFeedImageDataLoader in integration with the `CoreDataFeedStore delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
@@ -69,6 +69,18 @@ public final class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform { action(context) }
     }
+
+
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
 }
 
 extension CoreDataFeedStore: FeedImageDataStore {
